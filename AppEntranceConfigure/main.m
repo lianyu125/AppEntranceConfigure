9352cf6b75b58a9feeec9b7e88385add1a82bc0b
lianyu.zou <lianyu.zou@qunar.com>
2017-10-20 07:44:15 +0800

    创建工程

---
+++ AppEntranceConfigure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppEntranceCofigure
+//
+//  Created by one on 2017/10/20.
+//  Copyright © 2017年 ZLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
